e35858693b006f3e5cb272732b913bf7f3fa2fc0
Jack Preston <je.preston20@gmail.com>
2022-03-24 14:57:14 +0000

    Add date_calculations.js

---
+++ public/javascripts/date_calculations.js
@@ -0,0 +1,16 @@
+// When did the cycle start? That's week 1
+// seedDate should be the day before week 1 starts (e.g. Sunday 1st, if Monday 2nd is the first day of week 1)
+var seedDate = new Date('2022-03-20');
+var todayDate = new Date(Date.now());
+
+// What week is it this week?
+function weeksSinceSeedDate (todayDate, seedDate) {
+    // JavaScript stores date objects as milliseconds, so will need number of milliseconds in a day
+    const MILLISECONDS_IN_A_WEEK = 1000 * 60 * 60 * 24 * 7;
+
+    let differenceInWeeks = Math.floor((todayDate - seedDate) / MILLISECONDS_IN_A_WEEK);
+
+    return differenceInWeeks;
+};
+
+var weekNumber = weeksSinceSeedDate(todayDate, seedDate);
