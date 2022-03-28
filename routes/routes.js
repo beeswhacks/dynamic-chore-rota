dd46d9c7f7b32f407dde482e2ebad3f35c149d95
Jack Preston <je.preston20@gmail.com>
2022-03-28 14:37:08 +0100

    Split routing for each room

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,40 @@ var Task = require('../models/task.js');
 const { weekNumber } = require('../public/javascripts/week_number.js');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  Task.updateOne(
+    {weekNumber: weekNumber, room: 'Kitchen'}, 
+    {$set: {weekNumber: weekNumber, cleaner: 'Jack', room: 'Kitchen', status: false}},
+    {upsert: true},
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Updated Kitchen');
+      }
+    }
+    );
+
+  next();
+});
+
+router.get('/', function(req, res, next) {
+  Task.updateOne(
+    {weekNumber: weekNumber, room: 'Bathroom'}, 
+    {$set: {weekNumber: weekNumber, cleaner: 'Faith', room: 'Bathroom', status: false}},
+    {upsert: true},
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Updated Bathroom');
+      }
+    }
+    );
+    
+  next();
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // Try to find tasks with the corresponding week number
