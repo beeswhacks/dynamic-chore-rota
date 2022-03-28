e2001421499d9973784ffbca096102a44a19d819
Jack Preston <je.preston20@gmail.com>
2022-03-28 14:36:14 +0100

    Add index to schema

--- models/task.js
+++ models/task.js
@@ -2,10 +2,12 @@ var mongoose = require("mongoose");
 
 var taskSchema = mongoose.Schema({
     weekNumber: {type: Number, required: true},
-    cleaner: {type: String, required: true},
     room: {type: String, required: true},
+    cleaner: {type: String, required: true},
     status: {type: Boolean}
 });
+
+taskSchema.index({weekNumber: 1, room: 1})
 
 var Task = mongoose.model("Task", taskSchema);
 
